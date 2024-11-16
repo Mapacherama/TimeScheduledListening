32d66e4193db7c39bf42fdf3aa9ad9b336ebc705
Jerome Tesselaar <48964493+Mapacherama@users.noreply.github.com>
2024-11-16 10:27:07 +0100

    Fixing the bug.

--- main.py
+++ main.py
@@ -1,19 +1,23 @@
-from sched import scheduler
-from fastapi import FastAPI, Request, Query
+from fastapi import FastAPI, Request, HTTPException, Query
 from uvicorn import run
 from auth import callback, login
-from podcast import search_podcast
 from scheduled_playback import (
     initialize_spotify_client,
     refresh_token_if_needed,
-    play_playlist
+    play_playlist,
+    sp_oauth
 )
+from scheduler import schedule_playlist, start_scheduler, stop_scheduler
+from podcast import search_podcast
 import logging
-from scheduler import schedule_playlist
 
 app = FastAPI()
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+@app.on_event("startup")
+def startup_event():
+    start_scheduler()
 
 @app.get("/login")
 async def login_route():
@@ -25,21 +29,16 @@ async def callback_route(request: Request):
 
 @app.get("/schedule-playlist")
 def schedule_playlist_route(playlist_uri: str, play_time: str = Query(..., regex="^([0-9]{2}):([0-9]{2})$")):
-    return schedule_playlist(scheduler, play_playlist, playlist_uri, play_time)
+    return schedule_playlist(play_playlist, playlist_uri, play_time)
 
 @app.get("/search-podcast")
 def search_podcast_route(query: str):
     global sp
-    return search_podcast(sp, refresh_token_if_needed, query) 
-
-@app.on_event("startup")
-def startup_event():
-    initialize_spotify_client()
+    return search_podcast(sp, refresh_token_if_needed, query)
 
 @app.on_event("shutdown")
-async def shutdown_event():
-    logging.info("Shutdown event triggered.")
-    scheduler.shutdown(wait=False)
+def shutdown_event():
+    stop_scheduler()
 
 if __name__ == "__main__":
     run(app, host="0.0.0.0", port=8000, reload=True)
--- scheduler.py
+++ scheduler.py
@@ -1,15 +1,26 @@
 from apscheduler.triggers.date import DateTrigger
 from datetime import datetime
+from apscheduler.schedulers.background import BackgroundScheduler
 
-def schedule_playlist(scheduler, play_playlist, playlist_uri: str, play_time: str):
+scheduler = BackgroundScheduler()
+
+def start_scheduler():
+    scheduler.start()
+
+def stop_scheduler():
+    scheduler.shutdown()
+
+def schedule_playlist(play_playlist, playlist_uri: str, play_time: str):
     now = datetime.now()
     play_time_obj = datetime.strptime(play_time, "%H:%M").replace(
         year=now.year, month=now.month, day=now.day
     )
     if play_time_obj < now:
         play_time_obj = play_time_obj.replace(day=now.day + 1)
+    
     trigger = DateTrigger(run_date=play_time_obj)
     scheduler.add_job(play_playlist, trigger, args=[playlist_uri])
+    
     return {
         "message": f"Playlist {playlist_uri} scheduled to play at {play_time_obj.strftime('%Y-%m-%d %H:%M:%S')}"
     }
